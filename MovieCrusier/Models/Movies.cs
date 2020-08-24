14436c3cc8057b5b36f9f823ac04501b40c48312
DurgaBhavaniN <durgabhavanid347@gmail.com>
2020-08-24 14:54:40 +0530

    sharepoint

--- MovieCrusier/Models/Movies.cs
+++ MovieCrusier/Models/Movies.cs
@@ -1,16 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace MovieCrusier.Models
 {
     public partial class Movies
     {
         public int MovieId { get; set; }
+
+        [Required(ErrorMessage ="Please enter movie ")]
+        [StringLength(maximumLength: 20, MinimumLength = 4, ErrorMessage = "Name should between 4 to 20 characters")]
         public string MovieName { get; set; }
+        [Required(ErrorMessage ="Please enter BoxOffice range")]
         public string BoxOffice { get; set; }
+        [Required(ErrorMessage ="Select Yes or No")]
+
         public string Active { get; set; }
+        [Required(ErrorMessage ="Please select Genre")]
         public string Genre { get; set; }
+        [Required(ErrorMessage = "Please select HasTeaser")]
         public string HasTeaser { get; set; }
+        [Required(ErrorMessage = "Date is Required")]
         public DateTime DateOfLaunch { get; set; }
     }
 }
--- MovieCrusier/Models/User.cs
+++ MovieCrusier/Models/User.cs
@@ -1,15 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace MovieCrusier.Models
 {
-	public class Customer
+	public class User
 	{
-		public int UserId { get; set; }
+		[Required(ErrorMessage = "Enter UserName")]
 		public string UserName { get; set; }
+
+		[Required(ErrorMessage = "Enter Password")]
 		public string Password { get; set; }
-		public string City { get; set; }
+		
 	}
 }
