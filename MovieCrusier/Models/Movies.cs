ddcda5b54d4030afbcd64873c4214d17fa8cb976
DurgaBhavaniN <durgabhavanid347@gmail.com>
2020-08-24 11:01:59 +0530

    Sharepoint

---
+++ MovieCrusier/Models/Movies.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MovieCrusier.Models
+{
+    public partial class Movies
+    {
+        public int MovieId { get; set; }
+        public string MovieName { get; set; }
+        public string BoxOffice { get; set; }
+        public string Active { get; set; }
+        public string Genre { get; set; }
+        public string HasTeaser { get; set; }
+        public DateTime DateOfLaunch { get; set; }
+    }
+}
